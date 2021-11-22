554592a01416d4249171b93d48d062e33980de42
Omar <diopomar0902@gmail.com>
2021-11-22 16:53:10 +0100

    save

--- 18-11-2021 cours javascript/js/exo/if(exo).js
+++ 18-11-2021 cours javascript/js/exo/if(exo).js
@@ -2,16 +2,16 @@ var nom = "DIOP";
 var age = 23 ;
 
 if (age>=23){
-alert("vous etes majeur")
+alert("vous etes majeur");
 
 }
 
 if(nom != "omar") {
-    alert("acces interdit")
+    alert("acces interdit");
 }
 
-if(nom == "DIOP" && age  >=23  ) {
-    alert("tu est majeur parce que tu a 23 ans et ton nom est Diop")
+if(nom == "DIOP" && age  >=23) {
+    alert("tu est majeur parce que tu a 23 ans et ton nom est Diop");
 }
 
 
